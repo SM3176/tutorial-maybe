cf1814b8e48c14d9ee2ca57500678da399545104
SM3176 <202003176@student.polytechnic.bh>
2023-10-05 10:59:36 +0300

    Menu

---
+++ Assets/Script/Navigate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigate : MonoBehaviour
+{
+    public void goToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
